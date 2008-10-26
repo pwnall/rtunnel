9a49fc416f69ec03fb91f017b3c9eda9b3ebe944
steve <coderrr.contact@gmail.com>
2008-10-26 09:24:13 +0700

    moved leak testing into diff fle

---
+++ lib/leak.rb
@@ -0,0 +1,35 @@
+require 'core'
+require 'pp'
+
+class LeakTracker
+  def self.start
+    Thread.safe do
+      sleep 10
+      begin
+        objects = Hash.new 0
+
+        while true
+          last_objects = objects.dup
+          ObjectSpace.each_object do |o|
+            objects[o.class] += 1
+          end
+
+          objects.reject!{|k,v| ! last_objects.has_key? k }  unless last_objects.empty?
+
+          new_objects = objects.dup
+          objects.each do |(klass, count)|
+            new_objects.delete klass  if count < last_objects[klass]  # has been GC'ed, "cant be leaking"
+          end
+          objects = new_objects
+
+          PP.pp objects.sort_by{|(k,cnt)| cnt }.reverse[0..10], STDERR
+
+          sleep 10
+        end
+      rescue Object
+        STDERR.puts $!.inspect
+        STDERR.puts $!.backtrace.join("\n")
+      end
+    end
+  end
+end
