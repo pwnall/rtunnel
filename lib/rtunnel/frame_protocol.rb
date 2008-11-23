93037691cda97db0cbe45a52f9a1ceeeab615dca
steve <coderrr.contact@gmail.com>
2008-11-23 10:17:55 +0700

    used clearer variable name

--- lib/rtunnel/frame_protocol.rb
+++ lib/rtunnel/frame_protocol.rb
@@ -1,14 +1,14 @@
 # eventmachine protocol 
 module RTunnel::FrameProtocol
   def receive_data(data)
-    @incomplete_frame ||= ''
+    @frame_size_buffer ||= ''
 
     i = 0
     loop do
       while @frame_buffer.nil? and i < data.size
-        @incomplete_frame << data[i]
+        @frame_size_buffer << data[i]
         if (data[i] & 0x80) == 0
-          @remaining_frame_size = StringIO.new(@incomplete_frame).read_varsize
+          @remaining_frame_size = StringIO.new(@frame_size_buffer).read_varsize
           @frame_buffer = ''
         end
         i += 1
@@ -18,18 +18,17 @@ module RTunnel::FrameProtocol
       break  if @remaining_frame_size > data.size - i
 
       receive_frame(@frame_buffer + data[i, @remaining_frame_size])
-      @incomplete_frame, @frame_buffer = '', nil
+      @frame_size_buffer, @frame_buffer = '', nil
       i += @remaining_frame_size
     end
 
     @frame_buffer << data[i..-1]
     @remaining_frame_size -= data.size-i
   end
-  
+
   def send_frame(frame_data)
     size_str = StringIO.new
-    size_str.write_varsize frame_data.length
-    send_data size_str.string
-    send_data frame_data
+    size_str.write_varsize(frame_data.length)
+    send_data(size_str.string + frame_data)
   end
 end
