2faf2e1a7fd0f3eae6e6b927a4ba2277494657ab
steve <coderrr.contact@gmail.com>
2008-11-23 09:53:06 +0700

    refactored frame protocol receive_data

--- lib/rtunnel/frame_protocol.rb
+++ lib/rtunnel/frame_protocol.rb
@@ -1,39 +1,31 @@
 # eventmachine protocol 
 module RTunnel::FrameProtocol
   def receive_data(data)
-    @frame_incomplete ||= ''
+    @incomplete_frame ||= ''
 
     i = 0
     loop do
-      # read the frame size
-      while i < data.length and @frame_buffer.nil?
-        @frame_incomplete << data[i]
-        # completed frame        
+      while @frame_buffer.nil? and i < data.size
+        @incomplete_frame << data[i]
         if (data[i] & 0x80) == 0
-          @frame_incomplete = StringIO.new(@frame_incomplete).read_varsize
+          @remaining_frame_size = StringIO.new(@incomplete_frame).read_varsize
           @frame_buffer = ''
         end
         i += 1
       end
-      
-      break unless @frame_buffer
-     
-      if @frame_incomplete <= data.length - i
-        # break off frame
-        if @frame_buffer.empty?
-          receive_frame data[i, @frame_incomplete]
-        else
-          receive_frame @frame_buffer + data[i, @frame_incomplete]
-        end
-        i += @frame_incomplete
-        @frame_incomplete, @frame_buffer = '', nil
-      else
-        # buffer frame fragment
-        @frame_buffer << data[i..-1]
-        @frame_incomplete -= data.length - i
-        break
-      end
+
+      return  if @frame_buffer.nil?
+
+      remaining_bytes = data.size - i
+      break  if @remaining_frame_size > remaining_bytes
+
+      receive_frame(@frame_buffer + data[i, @remaining_frame_size])
+      @incomplete_frame, @frame_buffer = '', nil
+      i += @remaining_frame_size
     end
+
+    @frame_buffer << data[i..-1]
+    @remaining_frame_size -= data.size-i
   end
   
   def send_frame(frame_data)
